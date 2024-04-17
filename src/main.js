f6ffae8b53b779d7210160afd2266638e78cf271
Matis Leima <matisleima@gmail.com>
2024-04-17 18:27:10 +0300

    Esialgne landing page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // import your router configuration
+//
+// createApp(App)
+//     .use(router) // use the router
+//     .mount('#app');
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueAxios from "vue-axios";
+import axios from "axios";
+import "@/assets/css/my-style.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const app = createApp(App)
+
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+
+
+    <div class="container-fluid text-center vh-100">
+      <div class="row h-100">
+        <!-- LEFT SIDE -->
+        <div class="col align-items-center py-4">
+          <div class="col d-flex align-items-center justify-content-center py-4" style="min-height: 100vh;">
+            <h1>TEREH TULÕMAST SETOMAALÕ</h1>
+          </div>
+        </div>
+
+        <!-- RIGHT SIDE -->
+        <div class="col d-flex flex-column justify-content-between py-4">
+
+          <div class="row align-items-center" style="text-align: left; ">
+            <div class="col">
+              <img src="../assets/ait.webp" style="height: 100px; width: auto;" alt="Ait">
+            </div>
+            <div class="col">
+              <h5 style="margin-left: 10px;">AIT</h5>
+            </div>
+          </div>
+
+          <div class="row align-items-center" style="text-align: left;">
+            <div class="col">
+              <img src="../assets/kõlgus.png" style="height: 100px; width: auto;" alt="Kõlgus">
+            </div>
+            <div class="col">
+              <h5 style="margin-left: 10px;">KÕLGUS</h5>
+            </div>
+          </div>
+
+          <div class="row align-items-center" style="text-align: left;">
+            <div class="col">
+              <img src="../assets/plagandõ.png" style="height: 100px; width: auto;" alt="Plagandõ puhkemaja">
+            </div>
+            <div class="col">
+              <h5 style="margin-left: 10px;">PLAGANDÕ PUHKEMAJA</h5>
+            </div>
+          </div>
+
+          <div class="row align-items-center" style="text-align: left;">
+            <div class="col">
+              <img src="../assets/suvemaja.webp" style="height: 100px; width: auto;" alt="Suvemajake">
+            </div>
+            <div class="col">
+              <h5 style="margin-left: 10px;">SUVEMAJAKE</h5>
+            </div>
+          </div>
+
+          <div class="row align-items-center" style="text-align: left;">
+            <div class="col">
+              <img src="../assets/glämping.webp" style="height: 100px; width: auto;" alt="Glämping">
+            </div>
+            <div class="col">
+              <h5 style="margin-left: 10px;">GLÄMPING</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    goToView(viewName) {
+      this.$router.push({name: viewName});
+    }
+  }
+}
+
+</script>
+
