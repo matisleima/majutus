b6eb6a3c23c88445bca3ade30596de9344bfca52
Matis Leima <matisleima@gmail.com>
2024-04-23 00:02:34 +0300

    OK!!!

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,3 @@
-// import { fileURLToPath, URL } from 'node:url';
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-//
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   base: process.env.NODE_ENV === 'production' ? '/majutus/' : '/',
-//   build: {
-//     outDir: 'dist', // Default output directory
-//     rollupOptions: {
-//       output: {
-//         // Define naming convention for the assets
-//         assetFileNames: `assets/[name].[hash].[ext]`,
-//         chunkFileNames: `assets/[name].[hash].js`,
-//         entryFileNames: `assets/[name].[hash].js`,
-//       }
-//     }
-//   },
-//   resolve: {
-//     alias: {
-//       // Setup an alias for the src directory
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// });
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path'; // Make sure to import 'path'
