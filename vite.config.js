36121ec4460ba78fb904c9870edee9afffda3fdb
Matis Leima <matisleima@gmail.com>
2024-04-22 16:23:05 +0300

    updated main files

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ import path from 'path'; // Make sure to import 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/majutus/', // Set this to match your GitHub repository name
+  base: '/', // Set this to match your GitHub repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Correctly resolve '@' to the 'src' directory
