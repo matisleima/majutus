e6316e8cdea6d6e08ceb64a13c2f98e04dd24ddc
Matis Leima <matisleima@gmail.com>
2024-04-23 00:26:32 +0300

    let vue cal back in

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-// import { createApp } from 'vue';
-// import App from './App.vue';
-// import router from './router'; // import your router configuration
-//
-//
-//
-// import VueAxios from "vue-axios";
-// import axios from "axios";
-// import "@/assets/css/my-style.css"
-// import 'bootstrap/dist/css/bootstrap.min.css';
-//
-//
-// console.log('Creating app...');
-// const app = createApp(App)
-// console.log('App created.');
-//
-// app.use(router)
-// app.use(VueAxios, axios)
-// console.log('Mounting app...');
-// app.mount('#app')
-// console.log('App mounted.');
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
