a0a61cb2a6753cd36af507d92cc645fe205f2b4b
Matis Leima <matisleima@gmail.com>
2024-05-15 21:30:21 +0300

    Avalehe pildid hor aligned

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="section fixed-bg" :style="backgroundImageStyle">
+    <div class="section fixed-bg welcome-bg" :style="backgroundImageStyle">
       <h1 style="color: #ffffff; text-shadow: 2px 2px 4px rgba(0,0,0,0.9);">
         Kuninglik puhkus Setomaal
       </h1>
@@ -34,19 +34,18 @@
         </div>
       </div>
     </div>
-    <div class="section fixed-bg" :style="{ backgroundImage: 'url(' + aitImagePath + ')' }">
+    <div class="section fixed-bg ait-bg" :style="{ backgroundImage: 'url(' + aitImagePath + ')' }">
     </div>
     <div class="section scroll-bg">
       <div class="col">
         <img src="../assets/muu/map1.webp" width="700" style="margin-top: 20px"/></div>
-
       <div class="col">
         <p><h5> 35km raadiuses on hulk unikaalseid kohti: Võru linn, Vastseliina piiskopilinnus, Setomaa süda Obinitsa,
           Haanja ja Suur Munamägi, Rõuge ning Aluksne ja Põhja-Läti. Luha talu peremees sootska Aarne Leima on
           sertifitseeritud Setomaa giid ja näitab teile ümbruskonnas heameelega ringi!</h5></p>
       </div>
     </div>
-    <div class="section fixed-bg" :style="{ backgroundImage: 'url(' + saunImagePath + ')' }">
+    <div class="section fixed-bg saun-bg" :style="{ backgroundImage: 'url(' + saunImagePath + ')' }">
     </div>
     <div class="section scroll-bg">
       <contact-form/>
@@ -58,7 +57,7 @@
 <script>
 import welcomeImage from '@/assets/ait/welcome2.webp';
 import telkImage from '@/assets/glamping/glamping.webp';
-import saunImage from '@/assets/util/tiik-ava.webp';
+import saunImage from '@/assets/plagando/plagando-saun-3.webp';
 import ContactForm from "@/components/ContactForm.vue";
 
 export default {
@@ -123,6 +122,21 @@ export default {
 .row a:hover {
   color: #00b100; /* Lighter gray color on hover */
   text-decoration: underline; /* Adds underline on hover if desired */
+}
+
+/* Specific background adjustments for mobile */
+@media (max-width: 767px) {
+  .welcome-bg {
+    background-position: 100% center; /* Adjust as needed */
+  }
+
+  .ait-bg {
+    background-position: 35% center; /* Adjust as needed */
+  }
+
+  .saun-bg {
+    background-position: 63% center; /* Adjust as needed */
+  }
 }
 
 @media (min-width: 768px) {
