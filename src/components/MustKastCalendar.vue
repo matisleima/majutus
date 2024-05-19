0c4d868e816f5eac187d2cfeb8076bf720cfdee2
Matis Leima <matisleima@gmail.com>
2024-05-19 17:12:01 +0300

    Must kast kalender update. Glamping tuledega img add

--- src/components/MustKastCalendar.vue
+++ src/components/MustKastCalendar.vue
@@ -23,6 +23,10 @@ export default {
       currentMonth: new Date(),
       // Ensure the dates are strings and correctly formatted
       occupiedDates: [
+        '2024-06-14',
+        '2024-06-26',
+        '2024-06-27',
+        '2024-06-28',
         '2024-08-02',
         '2024-08-14',
         '2024-08-15',
