6c8dcc83d5952aef73b7431fac4bb0278da5c7ff
Matis Leima <matisleima@gmail.com>
2024-05-21 17:50:26 +0300

    Google Analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import App from './App.vue';
 import router from './router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueGtag from 'vue-gtag';
 
 const app = createApp(App);
+
+// Configure Google Analytics
+app.use(VueGtag, {
+    config: { id: 'G-YLXBHW1Z39' }
+}, router);
+
 app.use(router);
 app.use(VueAxios, axios);
 app.mount('#app');
